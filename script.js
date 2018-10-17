0200442692e36e664d25319fd169340cd8dd6b3e
Glitch (hello-webpage) <none>
2018-10-17 14:50:35 +0000

    🏞📟 Checkpoint ./script.js:378534/7644

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ var MOUSE_INFLUENCE = 1,
 
 var fluid = function () {  
     let ctx, width, height, num_x, num_y, particles, 
-        grid, meta_ctx, textures, num_particles
+        grid, metaCtx, textures, num_particles
     
     const threshold = 120
     const spacing = 315
@@ -16,7 +16,7 @@ var fluid = function () {
     const limit = radius * 0.56 
     
     const process = function () {
-        const imageData = meta_ctx.getImageData(0, 0, width, height)
+        const imageData = metaCtx.getImageData(0, 0, width, height)
         const pix = imageData.data
 
         for (let i = 0, n = pix.length; i < n; i += 4) {
@@ -27,147 +27,163 @@ var fluid = function () {
     }
 
     var run = function () {
-        meta_ctx.clearRect(0, 0, width, height);
+      metaCtx.clearRect(0, 0, width, height)
 
-        for (let i = 0, l = num_x * num_y; i < l; i++) {
-          grid[i].length = 0
-        }
-        
-        var i = num_particles;
-        while(i--) particles[i].first_process()
-        i = num_particles;
-        while(i--) particles[i].second_process()
+      for (let i = 0, l = num_x * num_y; i < l; i++) {
+        grid[i].length = 0
+      }
 
-        process()
+      let i = num_particles
+      
+      while (i--) {
+        particles[i].first_process()
+      }
+      
+      i = num_particles
+      
+      while (i--) {
+        particles[i].second_process()
+      }
 
-        const fillStyles = ['rgba(217, 160, 232, 0.005)', 
-                            'rgba(227, 50, 232, 0.005)', 
-                            'rgba(117, 160, 232, 0.005)']              
+      process()
 
-        fillStyles.map(f => {
-          ctx.fillStyle = f
-          ctx.beginPath()
-          ctx.arc(Math.random() * window.innerWidth, Math.random() * window.innerHeight,
-                  radius * MOUSE_INFLUENCE, 0, Math.PI * 2)
-          ctx.closePath()
-          ctx.fill()
-        })
+      const fillStyles = ['rgba(217, 160, 232, 0.005)', 
+                          'rgba(227, 50, 232, 0.005)', 
+                          'rgba(117, 160, 232, 0.005)']              
 
-       requestAnimationFrame(run);
+      fillStyles.map(f => {
+        ctx.fillStyle = f
+        ctx.beginPath()
+        ctx.arc(Math.random() * window.innerWidth, Math.random() * window.innerHeight,
+                radius * MOUSE_INFLUENCE, 0, Math.PI * 2)
+        ctx.closePath()
+        ctx.fill()
+      })
+
+     requestAnimationFrame(run);
     };
     
-    var Particle = function (type, x, y) {
-        this.type = type;
-        this.x = x;
-        this.y = y;
-        this.px = x;
-        this.py = y;
-        this.vx = 0;
-        this.vy = 0;
+    const Particle = function (type, x, y) {
+      this.type = type
+      this.x = x
+      this.y = y
+      this.px = x
+      this.py = y
+      this.vx = 0
+      this.vy = 0
     };
     
     Particle.prototype.first_process = function () {
-        var g = grid[Math.round(this.y / spacing) * num_x + Math.round(this.x / spacing)];
+      const g = grid[Math.round(this.y / spacing) * num_x + Math.round(this.x / spacing)]
 
-        if (g) {
-          g.close[g.length++] = this
-        }
+      if (g) {
+        g.close[g.length++] = this
+      }
 
-        this.vx = this.x - this.px
-        this.vy = this.y - this.py
-        
-        var dist_x = this.x - Math.random() * window.innerWidth
-        var dist_y = this.y - Math.random() * window.innerHeight
-        var dist = Math.sqrt(dist_x * dist_x + dist_y * dist_y)
-        if (dist < radius * MOUSE_INFLUENCE) {
-            var cos = dist_x / dist;
-            var sin = dist_y / dist;
-            this.vx += (MOUSE_REPEL) ? cos : -cos;
-            this.vy += (MOUSE_REPEL) ? sin : -sin;
-        }
+      this.vx = this.x - this.px
+      this.vy = this.y - this.py
 
-        this.vx += GRAVITY_X;
-        this.vy += GRAVITY_Y;
-        this.px = this.x;
-        this.py = this.y;
-        this.x += this.vx;
-        this.y += this.vy;
+      const distX = this.x - Math.random() * window.innerWidth
+      const distY = this.y - Math.random() * window.innerHeight
+      const dist = Math.sqrt(distX * distX + distY * distY)
+
+      if (dist < radius * MOUSE_INFLUENCE) {
+          const cos = distX / dist
+          const sin = distY / dist
+
+          this.vx += (MOUSE_REPEL) ? cos : -cos
+          this.vy += (MOUSE_REPEL) ? sin : -sin
+      }
+
+      this.vx += GRAVITY_X
+      this.vy += GRAVITY_Y
+      this.px = this.x
+      this.py = this.y
+      this.x += this.vx
+      this.y += this.vy
     };
         
     Particle.prototype.second_process = function () {
+      let forceA = 0
+      let forceB = 0
+      let cellX = Math.round(this.x / spacing)
+      let cellY = Math.round(this.y / spacing)
+      let close = []
 
-        var force = 0,
-            force_b = 0,
-            cell_x = Math.round(this.x / spacing),
-            cell_y = Math.round(this.y / spacing),
-            close = [];
-
-        for (var x_off = -1; x_off < 2; x_off++) {
-            for (var y_off = -1; y_off < 2; y_off++) {
-                var cell = grid[(cell_y + y_off) * num_x + (cell_x + x_off)];
-                if (cell && cell.length) {
-                    for (var a = 0, l = cell.length; a < l; a++) {
-                        var particle = cell.close[a];
-                        if (particle != this) {
-                            var dfx = particle.x - this.x;
-                            var dfy = particle.y - this.y;
-                            var distance = Math.sqrt(dfx * dfx + dfy * dfy);
-                            if (distance < spacing) {
-                                var m = 1 - (distance / spacing);
-                                force += Math.pow(m, 2);
-                                force_b += Math.pow(m, 3) / 2;
-                                particle.m = m;
-                                particle.dfx = (dfx / distance) * m;
-                                particle.dfy = (dfy / distance) * m;
-                                close.push(particle);
-                            }
-                        }
-                    }
+      for (let xOff = -1; xOff < 2; xOff++) {
+        for (let yOff = -1; yOff < 2; yOff++) {
+          const cell = grid[(cellY + yOff) * numX + (cellX + xOff)]
+          
+          if (cell && cell.length) {
+            for (var a = 0, l = cell.length; a < l; a++) {
+              const particle = cell.close[a]
+              
+              if (particle !== this) {
+                const dfx = particle.x - this.x
+                const dfy = particle.y - this.y
+                const distance = Math.sqrt(dfx * dfx + dfy * dfy)
+                
+                if (distance < spacing) {
+                  const m = 1 - (distance / spacing)
+                  forceA += Math.pow(m, 2)
+                  forceB += Math.pow(m, 3) / 2
+                  particle.m = m
+                  particle.dfx = (dfx / distance) * m
+                  particle.dfy = (dfy / distance) * m
+                  close.push(particle)
                 }
+              }
             }
+          }
+        }
+      }
+
+      force = (force - 3) * 0.35
+
+      for (var i = 0, l = close.length; i < l; i++) {
+        const neighbor = close[i]
+        const press = force + force_b * neighbor.m
+
+        if (this.type != neighbor.type) {
+          press *= 0.35
         }
 
-        force = (force - 3) * 0.35;
+        const dx = neighbor.dfx * press * 0.825
+        const dy = neighbor.dfy * press * 0.25
 
-        for (var i = 0, l = close.length; i < l; i++) {
+        neighbor.x += dx
+        neighbor.y += dy
+        this.x -= dx
+        this.y -= dy
+      }
 
-            var neighbor = close[i];
+      if (this.x < limit) {
+        this.x = limit
+      } else if (this.x > width - limit) {
+        this.x = width - limit
+      }
 
-            var press = force + force_b * neighbor.m;
-            if (this.type != neighbor.type) press *= 0.35;
+      if (this.y < limit) {
+        this.y = limit
+      } else if (this.y > height - limit) {
+        this.y = height - limit
+      }
 
-            var dx = neighbor.dfx * press * 0.825;
-            var dy = neighbor.dfy * press * 0.25;
-
-            neighbor.x += dx;
-            neighbor.y += dy;
-            this.x -= dx;
-            this.y -= dy;
-        }
-
-        if (this.x < limit) this.x = limit;
-        else if (this.x > width - limit) this.x = width - limit;
-
-        if (this.y < limit) this.y = limit;
-        else if (this.y > height - limit) this.y = height - limit;
-
-        this.draw();
+      this.draw()
     };
             
     Particle.prototype.draw = function () {
+      const size = radius * 2
 
-        var size = radius * 2;
-
-        meta_ctx.drawImage(
+      metaCtx.drawImage(
         textures[this.type],
         this.x - radius,
         this.y - radius,
         size,
-        size);
-    };
+        size)
+    }
         
     return {
-    
         init: function(canvas, w, h) {
 
             particles = [];
